079cb6567433f6e5ed37386ebe78b74e6b65e590
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-07-17 23:44:54 +0545

    inital commit

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final double price;
+
+  // ProductDetailScreen(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => ProductDetailScreen(title),
+              ),
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          title: Text(title, textAlign: TextAlign.center),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.shopping_cart),
+              color: Theme.of(context).accentColor),
+        ),
+      ),
+    );
+  }
+}
