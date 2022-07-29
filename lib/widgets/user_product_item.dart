2e3e24f8cf8fe8272beec6ef5bcd04f390fbcc6e
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-07-29 18:10:51 +0545

    added addproducts

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
   // const UserProductItem({ Key? key }) : super(key: key);
 
-final String title;
-final String ImageUrl;
+  final String id;
+  final String title;
+  final String ImageUrl;
 
-UserProductItem( 
-  this.title,
-  this.ImageUrl,
-);
+  UserProductItem(
+    this.id,
+    this.title,
+    this.ImageUrl,
+  );
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(title),
-      leading: CircleAvatar(backgroundImage: NetworkImage(ImageUrl),),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(ImageUrl),
+      ),
       trailing: Container(
         width: 100,
         child: Row(
           children: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.edit), color: Theme.of(context).primaryColor),
-            IconButton(onPressed: (){}, icon: Icon(Icons.delete), color: Theme.of(context).errorColor),
-            
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushNamed('/edit-product', arguments: id);
+                },
+                icon: Icon(Icons.edit),
+                color: Theme.of(context).primaryColor),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).errorColor),
           ],
         ),
       ),
-
-      
     );
   }
 }
