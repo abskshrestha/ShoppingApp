df5aa4a4be5daafeaa825ce532da7c56d076c066
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-08-06 16:13:23 +0545

    added fetchandsetup

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -24,26 +24,51 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    final url = Uri.parse(
+        'https://flutter-upgrade-3928f-default-rtdb.firebaseio.com/orders.json');
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderID, orderData) {
+      loadedOrders.add(OrderItem(
+          id: orderID,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map((item) => CartItem(
+                  id: item['id'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                  title: item['title']))
+              .toList()));
+    });
+  _orders = loadedOrders;
+  notifyListeners();
+  
+
+  }
+
+
   Future<void> addOrder(List<CartItem> CartProducts, double total) async {
     final url = Uri.parse(
         'https://flutter-upgrade-3928f-default-rtdb.firebaseio.com/orders.json');
-    final timeStamp = DateTime.now(); 
-    final response = await http.post(url, body: json.encode({
-      'amount': total,
-      'dateTime': timeStamp.toIso8601String(),
-      'product': CartProducts.map((cp) => {
-        'id': cp.id,
-        'title': cp.title,
-        'quantity': cp.quantity,
-        'price': cp.price,
-      }).toList(),
+    final timeStamp = DateTime.now();
+    final response = await http.post(url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timeStamp.toIso8601String(),
+          'product': CartProducts.map((cp) => {
+                'id': cp.id,
+                'title': cp.title,
+                'quantity': cp.quantity,
+                'price': cp.price,
+              }).toList(),
+        }));
 
-      }));
-
-
-      
-    
-  
     _orders.insert(
       0,
       OrderItem(
