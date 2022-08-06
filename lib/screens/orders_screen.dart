1fc15db7319cf7dae8e97ed2e4711d2fd1fd6317
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-08-06 16:12:34 +0545

    added fetc and setup

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
-import '../widgets/app_Drawer.dart';
-import 'package:flutter_complete_guide/widgets/order_item.dart';
 import 'package:provider/provider.dart';
+
 import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+import '../widgets/app_drawer.dart';
 
 class OrdersScreen extends StatelessWidget {
-  // const ({ Key? key }) : super(key: key);
-
   static const routeName = '/orders';
 
   @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
+    print('building orders');
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: ordersData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(ordersData.orders[i]),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              // ...
+              // Do error handling stuff
+              return Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
       ),
     );
   }
